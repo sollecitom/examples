305d324d3a1331af1acbdc41bc9bb08bacc59141
Michele Sollecito <msollecito@meta.com>
2025-11-12 11:48:39 +0000

    WIP

--- modules/exercises/event-framework/src/test/kotlin/sollecitom/examples/exercises/rpg/RolePlayingGamesTests.kt
+++ modules/exercises/event-framework/src/test/kotlin/sollecitom/examples/exercises/rpg/RolePlayingGamesTests.kt
@@ -15,8 +15,7 @@ import sollecitom.libs.swissknife.test.utils.execution.utils.test
 private class RolePlayingGamesTests : CoreDataGenerator by CoreDataGenerator.testProvider {
 
     // Tests To-Do List
-    // attribute-enhancing items
-    // temporary attribute enhancing items
+    // temporary attribute enhancing effects
     // skill checks
 
     @Nested
@@ -27,8 +26,8 @@ private class RolePlayingGamesTests : CoreDataGenerator by CoreDataGenerator.tes
 
             val challenge = newKnockDoorDownChallenge(difficultyClass = 13)
             val player = newPlayer(strength = 14)
-            val dice = loadedD20(result = 11)
 
+            val dice = loadedD20(result = 11)
             val outcome = player.attempt(challenge, dice) // 11 + 2 = 13 <= 13 => success
 
             assertThat(outcome).succeeded()
@@ -39,8 +38,8 @@ private class RolePlayingGamesTests : CoreDataGenerator by CoreDataGenerator.tes
 
             val challenge = newKnockDoorDownChallenge(difficultyClass = 13)
             val player = newPlayer(strength = 14)
-            val dice = loadedD20(result = 10)
 
+            val dice = loadedD20(result = 10)
             val outcome = player.attempt(challenge, dice) // 10 + 2 = 12 < 13 => failure
 
             assertThat(outcome).failed()
@@ -51,8 +50,8 @@ private class RolePlayingGamesTests : CoreDataGenerator by CoreDataGenerator.tes
 
             val challenge = newKnockDoorDownChallenge(difficultyClass = 35)
             val player = newPlayer(strength = 14)
-            val dice = loadedD20(result = 20)
 
+            val dice = loadedD20(result = 20)
             val outcome = player.attempt(challenge, dice) // 20 + 2 = 22 < 35 => critical success
 
             assertThat(outcome).criticallySucceeded()
@@ -63,11 +62,25 @@ private class RolePlayingGamesTests : CoreDataGenerator by CoreDataGenerator.tes
 
             val challenge = newKnockDoorDownChallenge(difficultyClass = 5)
             val player = newPlayer(strength = 20)
-            val dice = loadedD20(result = 1)
 
+            val dice = loadedD20(result = 1)
             val outcome = player.attempt(challenge, dice) // 1 + 5 = 6 >= 5 => fumble
 
             assertThat(outcome).fumbled()
+        }
+
+        @Test
+        fun `a player knocks a door down with help from a magical item`() = testWithGame(DungeonsAndDragons) {
+
+            val challenge = newKnockDoorDownChallenge(difficultyClass = 23)
+            val player = newPlayer(strength = 16)
+            val giantStrengthGauntlets = newStrengthEnhancingMagicalItem(bonus = 6)
+            player.equip(giantStrengthGauntlets)
+
+            val dice = loadedD20(result = 17)
+            val outcome = player.attempt(challenge, dice) // 17 + 3 + 3 (from +6 str bonus) = 23 <= 23 => success
+
+            assertThat(outcome).succeeded()
         }
 
         private fun DungeonsAndDragons.newKnockDoorDownChallenge(difficultyClass: Int = 10): DungeonsAndDragons.Challenge = newStrengthCheck(difficultyClass)
@@ -75,6 +88,8 @@ private class RolePlayingGamesTests : CoreDataGenerator by CoreDataGenerator.tes
         private fun DungeonsAndDragons.newStrengthCheck(difficultyClass: Int): DungeonsAndDragons.Challenge = DungeonsAndDragons.AttributeChallenge(difficultyClass = difficultyClass, attribute = DungeonsAndDragons.Attribute.STRENGTH)
 
         private fun DungeonsAndDragons.newPlayer(strength: Int = 10): DungeonsAndDragons.Player = DungeonsAndDragonsPlayerImplementation(strength = strength)
+
+        private fun DungeonsAndDragons.newStrengthEnhancingMagicalItem(bonus: Int): DungeonsAndDragons.Wearable = DungeonsAndDragonsAttributeEnhancingWearable(strength = bonus)
 
         private fun Assert<DungeonsAndDragons.Challenge.Outcome>.succeeded() = given { outcome ->
 
@@ -107,6 +122,18 @@ private class RolePlayingGamesTests : CoreDataGenerator by CoreDataGenerator.tes
     private fun loadedD20(result: Int): D20 = LoadedD20(result)
 }
 
+class DungeonsAndDragonsAttributeEnhancingWearable(private val strength: Int = 0, private val dexterity: Int = 0, private val constitution: Int = 0, private val intelligence: Int = 0, private val wisdom: Int = 0, private val charisma: Int = 0) : DungeonsAndDragons.Wearable.AttributeEnhancing {
+
+    override fun bonusToAttribute(attribute: DungeonsAndDragons.Attribute) = when (attribute) {
+        DungeonsAndDragons.Attribute.STRENGTH -> strength
+        DungeonsAndDragons.Attribute.DEXTERITY -> dexterity
+        DungeonsAndDragons.Attribute.CONSTITUTION -> constitution
+        DungeonsAndDragons.Attribute.INTELLIGENCE -> intelligence
+        DungeonsAndDragons.Attribute.WISDOM -> wisdom
+        DungeonsAndDragons.Attribute.CHARISMA -> charisma
+    }
+}
+
 interface D20 {
 
     fun roll(): Int
@@ -119,6 +146,8 @@ data class LoadedD20(private val result: Int) : D20 {
 
 class DungeonsAndDragonsPlayerImplementation(private val strength: Int) : DungeonsAndDragons.Player {
 
+    private val equippedItems = mutableSetOf<DungeonsAndDragons.Wearable>()
+
     init {
         require(strength >= 0) { "Strength must be greater or equal to zero" }
     }
@@ -126,6 +155,11 @@ class DungeonsAndDragonsPlayerImplementation(private val strength: Int) : Dungeo
     override fun attempt(challenge: DungeonsAndDragons.Challenge, dice: D20) = when (challenge) {
 
         is DungeonsAndDragons.AttributeChallenge -> attemptAttributeChallenge(challenge, dice)
+    }
+
+    override fun equip(wearable: DungeonsAndDragons.Wearable) {
+
+        equippedItems += wearable
     }
 
     private fun attemptAttributeChallenge(challenge: DungeonsAndDragons.AttributeChallenge, dice: D20): DungeonsAndDragons.Challenge.Outcome {
@@ -152,7 +186,17 @@ class DungeonsAndDragonsPlayerImplementation(private val strength: Int) : Dungeo
             DungeonsAndDragons.Attribute.WISDOM -> TODO("not implemented yet")
             DungeonsAndDragons.Attribute.CHARISMA -> TODO("not implemented yet")
         }
-        return score.modifier()
+        val bonus = bonus()
+        return (score + bonus).modifier()
+    }
+
+    private fun DungeonsAndDragons.Attribute.bonus() = equipmentBonus()
+
+    private fun DungeonsAndDragons.Attribute.equipmentBonus() = equippedItems.sumOf { it.bonusToAttribute(this) }
+
+    private fun DungeonsAndDragons.Wearable.bonusToAttribute(attribute: DungeonsAndDragons.Attribute): Int = when (this) {
+        is DungeonsAndDragons.Wearable.AttributeEnhancing -> bonusToAttribute(attribute)
+        else -> 0
     }
 
     private fun Int.modifier(): Int = (this - 10) / 2
@@ -163,6 +207,8 @@ object DungeonsAndDragons {
     interface Player {
 
         fun attempt(challenge: Challenge, dice: D20): Challenge.Outcome
+
+        fun equip(wearable: Wearable)
     }
 
     enum class Attribute {
@@ -182,6 +228,14 @@ object DungeonsAndDragons {
             data object Failure : Outcome
             data object Success : Outcome
             data object CriticalSuccess : Outcome
+        }
+    }
+
+    interface Wearable {
+
+        interface AttributeEnhancing : Wearable {
+
+            fun bonusToAttribute(attribute: Attribute): Int
         }
     }
 }
